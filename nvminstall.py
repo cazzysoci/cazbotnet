c438dc6276e5d67dd65554a74b1fa3a938a4c4f4
cazzysoci <carielglobio@gmail.com>
2024-11-19 20:57:25 +0800

    Update nvminstall.py

--- nvminstall.py
+++ nvminstall.py
@@ -6,4 +6,4 @@ os.system('sudo apt install curl ')
 os.system('curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash')
 os.system('nvm install --lts')
 
-print(f"\x1b[31m[\x1b[33mMedusa\x1b[31m]\x1b[0m \x1b[32m> \x1b[33mInstallation Successful")
+print(f"\x1b[31m[\x1b[33mcazbotnet\x1b[31m]\x1b[0m \x1b[32m> \x1b[33mInstallation Successful")
